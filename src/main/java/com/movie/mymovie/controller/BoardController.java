4675a0e35a707f3c7831290a6fd81d3ecbb8fc12
hyundai_edu301_13 <hyundai_edu301_13@DESKTOP-B69TSVS>
2020-06-11 17:46:54 +0900

    Board

--- src/main/java/com/movie/mymovie/controller/BoardController.java
+++ src/main/java/com/movie/mymovie/controller/BoardController.java
@@ -1,5 +1,19 @@
 package com.movie.mymovie.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BoardController {
 
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@RequestMapping(value = "board/BoardList")
+	public String doBoardList() {
+		logger.info("BoardList");
+		
+		return "board/BoardList";
+	}
 }
