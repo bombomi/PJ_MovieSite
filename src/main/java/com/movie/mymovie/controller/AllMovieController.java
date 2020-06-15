8d3d6414dddd8b3e9c6e2849ffdb51b66f13b340
hyundai_edu301_13 <hyundai_edu301_13@DESKTOP-B69TSVS>
2020-06-15 10:31:53 +0900

    김보민 pull 받음

--- src/main/java/com/movie/mymovie/controller/AllMovieController.java
+++ src/main/java/com/movie/mymovie/controller/AllMovieController.java
@@ -4,20 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AllMovieController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AllMovieController.class);
-	
+
 	@RequestMapping(value = "main/index")
 	public String doIndex() {
 		logger.info("main/index");
 		return "main/index";
 	}
 
-	
-	 
-	
 }
